13bfa62baf06eff25211f1cd8db98736a78ea192
cchao2869 <cchao2869@charlottesvilleschools.org>
2023-08-31 11:30:53 -0400

    Servo stop after no touch

--- servo_cap.py
+++ servo_cap.py
@@ -11,17 +11,20 @@ touch_A1 = touchio.TouchIn(board.A1)
 touch_A2 = touchio.TouchIn(board.A2) 
 pwm = pwmio.PWMOut(board.A4, duty_cycle=2 ** 15, frequency=50)
 my_servo = servo.Servo(pwm)
+angle = 1
 
 
 while True:
     if touch_A1.value:
-        for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
-            my_servo.angle = angle
-            time.sleep(0.05)
-            print(angle)
+            if angle < 180:
+                angle = angle + 5
+                my_servo.angle = angle 
+                time.sleep(0.05)
+                print(angle)
     if touch_A2.value:
-        for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
-            my_servo.angle = angle
-            time.sleep(0.05)
-            print(angle)
+            if angle > 0:
+                angle = angle - 5
+                my_servo.angle = angle 
+                time.sleep(0.05)
+                print(angle)
     time.sleep(0.05)
