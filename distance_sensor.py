68314f473d901d85dcb5571ce8eaa878c3effbc3
cchao2869 <cchao2869@charlottesvilleschools.org>
2023-09-08 11:21:36 -0400

    renamed readme

--- distance_sensor.py
+++ distance_sensor.py
@@ -7,20 +7,26 @@ import board
 import adafruit_hcsr04
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
 
-from rainbowio import colorwheel
-import neopixel
-
 import neopixel
 led = neopixel.NeoPixel(board.NEOPIXEL, 1)
 led.brightness = (0.3)
 
 
 while True:
-    print((sonar.distance,))
-    time.sleep(0.1)
+    try:
 
-    if sonar.distance < 34:
-        led[0] = (255, 0, 0)
-    
-    if sonar.distance > 34:
-        led[0] = (0, 255, 0)
+        if sonar.distance < 34:
+            led[0] = (255, 0, 0)
+            print(sonar.distance, "red")
+            time.sleep(0.1)
+
+        if sonar.distance > 34:
+            led[0] = (0, 255, 0)
+            print(sonar.distance, "green")
+            time.sleep(0.1)
+
+    except RuntimeError:
+        print("Retrying!")
+        pass
+    time.sleep(0.1)
+   
