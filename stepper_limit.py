ad879fb1dbf5766c8ab43d625a076765cec5bac5
cchao2869 <cchao2869@charlottesvilleschools.org>
2024-01-12 11:13:07 -0500

    add limit switch

--- stepper_limit.py
+++ stepper_limit.py
@@ -21,6 +21,18 @@ for coil in coils:          # set digital pins as output
 
 motor = stepper.StepperMotor(coils[0], coils[1], coils[2], coils[3], microsteps=None)   # motor object created
 
+async def catch_pin_transitions(pin):
+    # Print a message when pin goes low and when it goes high.
+    with keypad.Keys((pin,), value_when_pressed=False) as keys:
+        while True:
+            event = keys.events.get()
+            if event:
+                if event.pressed:
+                    print("Limit Switch was pressed.")
+                elif event.released:
+                    print("Limit Switch was released.")
+            await asyncio.sleep(0)
+
 while True:
     for step in range(STEPS):       # 
         motor.onestep(style=stepper.DOUBLE)     # move motor clockwise
@@ -29,3 +41,9 @@ while True:
     for step in range(STEPS):
         motor.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)     # move motor counterclockwise and put  motor in  setting for  highest torque for pressing down the limit switch
         time.sleep(DELAY)
+
+async def main():
+    interrupt_task = asyncio.create_task(catch_pin_transitions(board.D2))
+    await asyncio.gather(interrupt_task)
+
+asyncio.run(main())
