381133dec89b9ddbe0d6fc3f54cff8ea372f1aed
cchao2869 <cchao2869@charlottesvilleschools.org>
2024-01-17 12:43:30 -0500

    limit switch motor function update

--- stepper_limit.py
+++ stepper_limit.py
@@ -1,10 +1,13 @@
 # Carolina Chao
 # Stepper Mottor and Limit Switch - 1/10/2024
 
+import asyncio
 import board
+import keypad
 import time
 import digitalio
 from adafruit_motor import stepper
+
 
 DELAY = 0.01    # variable for time between steps
 STEPS = 100     # 100 = 0.5 times full rotation of motor
@@ -21,8 +24,7 @@ for coil in coils:          # set digital pins as output
 
 motor = stepper.StepperMotor(coils[0], coils[1], coils[2], coils[3], microsteps=None)   # motor object created
 
-async def catch_pin_transitions(pin):
-    # Print a message when pin goes low and when it goes high.
+async def catch_pin_transitions(pin):      # Print a message when pin goes low and when it goes high.
     with keypad.Keys((pin,), value_when_pressed=False) as keys:
         while True:
             event = keys.events.get()
@@ -31,19 +33,31 @@ async def catch_pin_transitions(pin):
                     print("Limit Switch was pressed.")
                 elif event.released:
                     print("Limit Switch was released.")
-            await asyncio.sleep(0)
+            await asyncio.sleep(0)       # run motor to move to check switch again
 
-while True:
-    for step in range(STEPS):       # 
-        motor.onestep(style=stepper.DOUBLE)     # move motor clockwise
-        time.sleep(DELAY)
+async def run_motor():
+    while (True):
+        for step in range(STEPS):       # 
+            motor.onestep(style=stepper.DOUBLE)     # move motor clockwise
+            time.sleep(DELAY)
 
-    for step in range(STEPS):
-        motor.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)     # move motor counterclockwise and put  motor in  setting for  highest torque for pressing down the limit switch
-        time.sleep(DELAY)
+        for step in range(STEPS):
+            motor.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)     # move motor counterclockwise and put  motor in  setting for  highest torque for pressing down the limit switch
+            time.sleep(DELAY)
+
+        await asyncio.sleep(0)       # run limit switch values to move motor again
+
 
 async def main():
-    interrupt_task = asyncio.create_task(catch_pin_transitions(board.D2))
-    await asyncio.gather(interrupt_task)
+    while (True):
+        interrupt_task = asyncio.create_task(catch_pin_transitions(board.D2))
+        motor_task = asyncio.create_task(run_motor(board.D9, board.D10, board.D11, board.D12))          # create motor task that calls run_motor to monitor motor value changes
+        await asyncio.gather(interrupt_task, motor_task)
 
 asyncio.run(main())
+
+
+        
+
+
+
